edd6f905428f6a7cce5c772cb411c73a1cf513d0
hsinyun <ruby8941@gmail.com>
2022-03-05 17:00:47 +0800

    [uuid] install uuid

--- server.js
+++ server.js
@@ -1,10 +1,46 @@
-console.log('object');
 const http = require('http');
+const toDoListService = require('./helpers/to_do_list');
 
 const requestListener = function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'Text/plain' });
-  res.write('hello world');
-  res.end();
+  const headers = {
+    'Access-Control-Allow-Headers':
+      'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+    'Content-Type': 'application/json',
+  };
+
+  if (req.url == '/todolist') {
+    toDoListService.test();
+    res.writeHead(200, { 'Content-Type': 'Text/plain' });
+    res.write('hello world');
+    res.end();
+  } else if (req.url == '/to-do-item' && req.method == 'GET') {
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: 'success',
+        data: [
+          {
+            id: 1,
+          },
+        ],
+      }),
+    );
+    res.end();
+  } else if (req.method === 'OPTIONS') {
+    res.writeHead(200, headers);
+    res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write(
+      JSON.stringify({
+        status: 'failed',
+        message: 'page not found',
+      }),
+    );
+    res.end();
+  }
 };
 
 const server = http.createServer(requestListener);
