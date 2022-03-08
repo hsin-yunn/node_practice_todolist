2773f619a79d0b3539ca336c22c41db6b6631e0b
hsinyun <ruby8941@gmail.com>
2022-03-09 01:05:43 +0800

    [todo] add delete all / delete one , add errorHelper

--- helpers/to_do_list.js
+++ helpers/to_do_list.js
@@ -1,5 +1,7 @@
+const { v4: uuidv4 } = require('uuid');
 module.exports = {
   test: function () {
     console.log('test helper');
+    console.log(uuidv4());
   },
 };
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http = require('http');
-const toDoListService = require('./helpers/to_do_list');
+const { v4: uuidv4 } = require('uuid');
+const errorHelper = require('./helpers/errorHelper');
+
+const todos = [];
 
 const requestListener = function (req, res) {
   const headers = {
@@ -10,12 +13,25 @@ const requestListener = function (req, res) {
     'Content-Type': 'application/json',
   };
 
-  if (req.url == '/todolist') {
-    toDoListService.test();
-    res.writeHead(200, { 'Content-Type': 'Text/plain' });
-    res.write('hello world');
+  // body
+  let body = '';
+  req.setEncoding('utf8');
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+  //api
+  if (req.url == '/todos' && req.method == 'GET') {
+    //get all todo
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: 'success',
+        data: todos,
+      }),
+    );
     res.end();
-  } else if (req.url == '/to-do-item' && req.method == 'GET') {
+  } else if (req.url == '/todo' && req.method == 'GET') {
+    // show todo
     res.writeHead(200, headers);
     res.write(
       JSON.stringify({
@@ -28,7 +44,58 @@ const requestListener = function (req, res) {
       }),
     );
     res.end();
+  } else if (req.url == '/todo' && req.method == 'POST') {
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+        if (!data.title) {
+          // title required
+          errorHelper.todoCreateErrHandle(res);
+        } else {
+          res.writeHead(201, headers);
+          todos.push({
+            id: uuidv4(),
+            title: data.title,
+          });
+          res.write(JSON.stringify(todos));
+          res.end();
+        }
+      } catch (err) {
+        errorHelper.todoCreateErrHandle(res);
+      }
+    });
+  } else if (req.url == '/todos' && req.method == 'DELETE') {
+    //delete all todo
+    todos.length = 0;
+    res.writeHead(200, headers);
+    res.write(
+      JSON.stringify({
+        status: 'success',
+        message: 'todos deleted',
+        data: todos,
+      }),
+    );
+    res.end();
+  } else if (req.url.startsWith('/todo/') && req.method == 'DELETE') {
+    // delete one todo
+    const id = req.url.split('/').pop();
+    const index = todos.findIndex((item) => item.id === id);
+    if (index > -1) {
+      todos.splice(index, 1);
+      res.writeHead(200, headers);
+      res.write(
+        JSON.stringify({
+          status: 'success',
+          message: 'data is deleted',
+          data: todos,
+        }),
+      );
+      res.end();
+    } else {
+      errorHelper.todoDeleteErrHandle(res);
+    }
   } else if (req.method === 'OPTIONS') {
+    //OPTIONS
     res.writeHead(200, headers);
     res.end();
   } else {
